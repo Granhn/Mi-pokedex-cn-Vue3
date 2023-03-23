6bf5668dcf6ae35bfe22d4cd8d8f495867bb576b
Granhn <fuenzaladeveloper@gmail.com>
2023-03-22 22:32:12 -0300

    Se anniadio el uso de local storage para guardar los pokemones favoritos

--- src/stores/usePokemonStore.js
+++ src/stores/usePokemonStore.js
@@ -5,14 +5,21 @@ import { useGetData } from "../composables/getData";
 export const usePokemonStore = defineStore('alerts', () =>{
 
   let favoritos = ref([]);
+  if (localStorage.getItem('favoritos') ){
+    favoritos.value = JSON.parse(localStorage.getItem('favoritos'))
+  }
+
   const addAFavoritos = (nombrePokemon,spritePokemon) =>{
     favoritos.value.push({nombrePokemon, spritePokemon});
+    localStorage.setItem('favoritos', JSON.stringify(favoritos.value));
   }
+
   const encontrarFavorito = (nombrePokemon) => {
     const existeEnFavoritos = favoritos.value.some(pokemon => pokemon.nombrePokemon === nombrePokemon);
     if (existeEnFavoritos) return true
     return false
   }
+
   const removerFavorito = (pokemonName) => {
     favoritos.value = favoritos.value.filter(pokemon => pokemon.nombrePokemon !== pokemonName)
   }
